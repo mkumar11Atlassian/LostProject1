840c43b5a7715efa9be838e5acf10478749f3625
Mukesh Kumar <mkumar11@atlassian.com>
2024-12-16 18:09:06 +0530

    Step3: code further simplified (+2 squashed commit)
    Squashed commit:
    
    [f28682b] Step2: Removed the empty lines from code.
    
    [a4804d3] Code added to multiply 3 numbers

--- xamls/AtlassianAccountStagePanel.xaml.cs
+++ xamls/AtlassianAccountStagePanel.xaml.cs
@@ -25,6 +25,18 @@ namespace SourceTree.UI.View.Welcome
             InitializeComponent();
 
             DomainDetails.IsVisibleChanged += DomainDetails_IsVisibleChanged;
+
+            // Prompt the user to enter three numbers
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            // Calculate the product of the three numbers
+            //double product = num1 * num2 * num3;
+            // Display the result
+            Console.WriteLine("The product of the three numbers is: " + num1 * num2 * num3/*product*/);
         }
 
         private void DomainDetails_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
